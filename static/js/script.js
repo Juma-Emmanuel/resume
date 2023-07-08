25d39449672138dd9bd615a249486be5272c097e
Emmanuel Juma <jumaemmanuel462@yahoo.com>
2023-07-08 21:03:50 +0300

    update

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,6 @@ var typingEffect = new Typed('.multiText', {
     strings: ['Project manager', 'Data analyst'],
     typeSpeed: 100,
     backSpeed: 100,
-    backDelay: 3000,
+    backDelay: 100,
     loop: true
 });
