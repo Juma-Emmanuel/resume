247a3df437d785b61e10fa9cb5835246f8f6fc21
Emmanuel juma <jumaemmanuel462@yahoo.com>
2023-07-12 22:42:16 +0300

    update

--- djangoresume/settings.py
+++ djangoresume/settings.py
@@ -116,16 +116,6 @@ os.path.join(BASE_DIR, 'static'),
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import pyrebase
 
-config={
-     "apiKey": "AIzaSyCeyATBXwuySMsCGxIk-7rUV-mS3p-qyE4",
-    "authDomain": "contactform-fc473.firebaseapp.com",
-    "databaseURL": "https://contactform-fc473-default-rtdb.firebaseio.com",
-    "projectId": "contactform-fc473",
-    "storageBucket": "contactform-fc473.appspot.com",
-    "messagingSenderId": "1035070557982",
-    "appId": "1:1035070557982:web:7094306e82429d3961430f"
-}
-firebase=pyrebase.initialize_app(config)
+
 
--- resumesite/views.py
+++ resumesite/views.py
@@ -1,25 +1,21 @@
 from django.shortcuts import render
-from firebase_admin import firestore
 import pyrebase
-config={
-     "apiKey": "AIzaSyCeyATBXwuySMsCGxIk-7rUV-mS3p-qyE4",
+
+config = {
+   " apiKey": "AIzaSyCeyATBXwuySMsCGxIk-7rUV-mS3p-qyE4",
     "authDomain": "contactform-fc473.firebaseapp.com",
     "databaseURL": "https://contactform-fc473-default-rtdb.firebaseio.com",
-    "projectId": "contactform-fc473",
+   " projectId": "contactform-fc473",
     "storageBucket": "contactform-fc473.appspot.com",
     "messagingSenderId": "1035070557982",
-    "appId": "1:1035070557982:web:7094306e82429d3961430f"
-}
-firebase=pyrebase.initialize_app(config)
-# Create your views here.
-def home(request):
-     name = request.POST.get("name")
-     email = request.POST.get("email")
-     message = request.POST.get("message")
+   " appId": "1s:1035070557982:web:7094306e82429d3961430f"
+  }
 
-     firebase.database().child("contactForm").push({
-    "name": name,
-    "email": email,
-    "message": message,
-     })
+ firebase = pyrebase.initializeApp(config)
+
+ auth = firebase.auth()
+def home(request):
+   
+
+    
      return render(request, 'home.html',{})
